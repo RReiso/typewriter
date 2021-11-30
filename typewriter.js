a100439662a5bd80be8345d77887a09f70846076
Ruta Reisoglu <rutareiso@protonmail.com>
2021-11-30 05:16:27 +0000

    Write sentence letter by letter with delay to stdout

---
+++ typewriter.js
@@ -0,0 +1,9 @@
+const sentence = "hello there from lighthouse labs";
+
+for (const i in sentence) {
+  setTimeout(()=>{
+    process.stdout.write(sentence[i]);
+    if (parseInt(i) === sentence.length - 1) process.stdout.write("\n");
+  },50 * i);
+}
+
